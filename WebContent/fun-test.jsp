58c206f1a7fd7d577e6f432a4912dc60db9902b3
Alexander Hartson <alexander-hartson@live.com>
2018-05-21 14:47:49 -0500

    adding files

---
+++ WebContent/fun-test.jsp
@@ -0,0 +1,9 @@
+<%@ page import="com.luv2code.jsp.*" %>
+<html>
+
+<body>
+
+Let's have some fun: <%= FunUtils.makeItLower("FUN FUN FUN") %>
+</body>
+
+</html>
--- src/com/luv2code/jsp/FunUtils.java
+++ src/com/luv2code/jsp/FunUtils.java
@@ -1,5 +1,8 @@
 package com.luv2code.jsp;
 
 public class FunUtils {
-
+	
+	public static String makeItLower(String data) {
+		return data.toLowerCase();
+	}
 }
